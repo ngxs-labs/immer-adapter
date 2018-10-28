37cddecd80de3056c15ba718ccf2bb0ef88e4329
Artur Androsovych <arthurandrosovich@gmail.com>
2018-10-28 11:09:31 +0200

    feat(immer-adapter): prepare for first publish (#2)

--- src/lib/core/internal/internals.ts
+++ src/lib/core/internal/internals.ts
@@ -1,15 +1,21 @@
 import { StateContext } from '@ngxs/store';
 
 /**
- * An utility function
+ * An utility function, that determines if provided target is defined and not nullable
  *
  * @param target - Value to check for being `undefined` or `null`
  * @returns - True if the passed target is not `undefined` and not `null`
  */
-function isDefined<T>(target: T): boolean {
+function isDefined<T = unknown>(target: T): boolean {
     return target !== undefined && target !== null;
 }
 
-export function isValidContext<T = any>(ctx: StateContext<T>): boolean {
+/**
+ * An utility function, that determines if provided object is valid
+ *
+ * @param ctx - `StateContext` plain object
+ * @returns - True if provided object is valid and has necessary methods
+ */
+export function isValidContext<T = unknown>(ctx: StateContext<T>): boolean {
     return isDefined(ctx) && typeof ctx.getState === 'function' && typeof ctx.setState === 'function';
 }
