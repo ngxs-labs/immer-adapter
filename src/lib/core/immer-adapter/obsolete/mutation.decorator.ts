6b8c1719aafa034c4b7fbb3df662683b216e53cf
splincode <omaxphp@yandex.ru>
2019-04-09 00:57:05 +0300

    chore: mark as deprecated decorator

--- src/lib/core/immer-adapter/obsolete/mutation.decorator.ts
+++ src/lib/core/immer-adapter/obsolete/mutation.decorator.ts
@@ -1,12 +1,16 @@
 import { StateContext } from '@ngxs/store';
-import { ImmutableContext } from 'immer-adapter/lib/core/immer-adapter/common/immutable-state-context';
+import { ImmutableStateContext } from '../common/immutable-state-context';
 
+/**
+ * @deprecated - use ImmutableContext instead Mutation
+ * @constructor
+ */
 export function Mutation(): Function {
   return function(_target: Object, _key: string, descriptor: PropertyDescriptor): PropertyDescriptor {
     const method = descriptor.value;
 
     descriptor.value = function(ctx: StateContext<any>, action: any, ...args: any[]) {
-      return method.apply(this, [new ImmutableContext(ctx), action, ...args]);
+      return method.apply(this, [new ImmutableStateContext(ctx), action, ...args]);
     };
 
     return descriptor;
