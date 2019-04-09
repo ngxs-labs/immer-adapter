7072eb1be70e84a44978d45d18cf8220ecb4a154
splincode <omaxphp@yandex.ru>
2019-04-09 08:50:32 +0300

    fix: expose public API

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,1 +1,3 @@
 export * from './lib/core/immer-adapter/obsolete';
+export { ImmutableContext } from './lib/core/immer-adapter/decorators/immutable-context.decorator';
+export { ImmutableSelector } from './lib/core/immer-adapter/decorators/immutable-selector.decorator';
